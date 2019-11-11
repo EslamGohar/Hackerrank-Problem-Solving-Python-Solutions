2b2a03eeae866392332f21da54b8b210a6a27743
Eslam Gohar <islamgoher1@gmail.com>
2019-11-11 19:46:14 +0200

    Update Plus_Minus.py
    *NOTE* This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with an absolute error of up to (10^ -4) are acceptable.

--- HackerRank-Plus Minus/Plus_Minus.py
+++ HackerRank-Plus Minus/Plus_Minus.py
@@ -8,23 +8,33 @@ import sys
 
 # Complete the plusMinus function below.
 def plusMinus(arr):
-    pos=0
-    neg=0
-    neu=0
+    pos = 0
+    neg = 0
+    zer = 0
+    
     for i in range(len(arr)):
-        if(arr[i]>0):
-            pos+=1
-        elif(arr[i]==0):
-            neu+=1
+        if arr[i] > 0 :
+            pos += 1
+        elif arr[i] == 0:
+            zer += 1
         else:
-            neg+=1
-    print(pos/len(arr))
-    print(neg/len(arr))
-    print(neu/len(arr))
+            neg += 1
+            
+    fractPos = pos / len(arr)
+    fractNeg = neg / len(arr)
+    fractZer = zer / len(arr)
+    
+    # here's the scale to six decimal places
+    ratioPos = format(fractPos, '.6f')
+    ratioNeg = format(fractNeg, '.6f')
+    ratioZer = format(fractZer, '.6f')
+
+    print(ratioPos)
+    print(ratioNeg)
+    print(ratioZer)
 
 if __name__ == '__main__':
     n = int(input())
-
     arr = list(map(int, input().rstrip().split()))
 
     plusMinus(arr)
